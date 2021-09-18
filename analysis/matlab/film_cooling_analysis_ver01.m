372260151195dbd8e209fd80ab9725783f8abe4f
Tom Franklyn Gammage <tomfranklyngammage@nadata.co.uk>
2021-09-18 19:37:59 +0100

    Wrote the boundary conditions and mesh sections of Chapter 4. Less review of the film cooling model was required than previously thought, as the required formulations may be derived from principles aleady discussed in the thesis. The Matlab script is ready to specify a plenum total pressure in the CFD simulations to be performed tomorrow. This should allow for most of Sunday and all of Monday to be devoted to analysis and discussion of results.

--- analysis/matlab/film_cooling_analysis_ver01.m
+++ analysis/matlab/film_cooling_analysis_ver01.m
@@ -14,7 +14,7 @@ p_w = 3085900;
 
 coolant_ratio = 0.008;
 
-T_0C = 300;
+T_0c = 300;
 
 m_dot_main = 120;
 
@@ -23,11 +23,6 @@ m_dot_coolant = m_dot_main*coolant_ratio;
 plenum_area = 7.4993e-4;
 
 zeta_p = 0.7;
-
-A = 2*(gamma-1)/gamma;
-B = p_w^((gamma-1)/gamma);
-C = (1-gamma)/gamma;
-D = m_dot_coolant^2/( plenum_area^2*p_w^(2/gamma)*( 2*gamma/(R*T_0C*(gamma-1)) ) );
 
 %iteratively solve for the plenum total pressure
 
@@ -40,8 +35,8 @@ array = zeros(size,1);
 p_0c = p_w;
 
 for i = 1:size
-   p_0c = ( (1/B)*( 1 - D/(p_0c^A) ) )^(1/C);
-   array(i,1) = p_0c;
+  p_0c = p_w*( 1 - ( R*(gamma-1)*T_0c*m_dot_coolant^2*p_0c^((2-2*gamma)/gamma) )/( 2*gamma*plenum_area^2*p_w^(2/gamma) ) )^(gamma/(1-gamma));
+  array(i,1) = p_0c;
 end
 
 ratio_of_coolant_to_main_tp = p_0c/p_w;
@@ -57,6 +52,6 @@ ratio_of_plenum_to_main_tp = p_0cp/p_w;
 
 
 figure(1)
-plot(array(:,1))
+plot(array(:,1)) 
 
 
